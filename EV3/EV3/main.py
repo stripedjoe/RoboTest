4742e311562ef9cfc18499755b4870fe2e26e666
Chito <remote.alfon@gmail.com>
2024-06-25 01:30:14 +0800

    suc1

--- EV3/EV3/main.py
+++ EV3/EV3/main.py
@@ -7,14 +7,12 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 
 # Create your objects here.
 ev3 = EV3Brick()
-
 
 # Write your program here.
 ev3.speaker.beep()
