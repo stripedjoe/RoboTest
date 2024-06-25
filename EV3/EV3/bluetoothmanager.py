c0c413ef3b8f2be48faa73e57108584e00e82854
Chito <remote.alfon@gmail.com>
2024-06-25 10:45:23 +0800

    save

--- EV3/EV3/bluetoothmanager.py
+++ EV3/EV3/bluetoothmanager.py
@@ -1,9 +1,26 @@
 from pybricks.hubs import EV3Brick
+from pybricks.messaging import BluetoothMailboxServer, TextMailbox, BluetoothMailboxClient
 
 
 class BluetoothManager:
     @staticmethod
-    def listen_to_message():
-        # ToDo. to be changed soon, https://pybricks.com/ev3-micropython/messaging.html
-        ev3 = EV3Brick()
-        ev3.speaker.beep()
+    def listen_to_message() -> str:
+        # # ToDo. to be changed soon, https://pybricks.com/ev3-micropython/messaging.html
+        # ev3 = EV3Brick()
+        # ev3.speaker.beep()
+
+        SERVER = 'ev3dev'
+        client = BluetoothMailboxClient()
+        mbox = TextMailbox('greeting', client)
+
+        print('establishing connection...')
+        client.connect(SERVER)
+        print('connected!')
+
+        # In this program, the client sends the first message and then waits for the
+        # server to reply.
+        mbox.send('hello!')
+        mbox.wait()
+        message_received = mbox.read()
+        print(mbox.read())
+        return message_received
